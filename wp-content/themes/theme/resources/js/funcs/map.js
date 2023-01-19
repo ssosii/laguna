1ddc91b176000b5e2d79f74c0b0d0f001cd7be91
Maciej Figlarz <maciejfiglarz333@gmail.com>
2023-01-19 16:48:34 +0100

    update

--- wp-content/themes/theme/resources/js/funcs/map.js
+++ wp-content/themes/theme/resources/js/funcs/map.js
@@ -1,17 +1,29 @@
 const map = () => {
   const listItems = document.querySelectorAll("#map .list .item");
+  const mapItems = document.querySelectorAll("#map .map .st0");
   const svgMap = document.querySelector("#svg2");
 
-  if(!svgMap) return;
-  console.log("svg",listItems);
+  if (!svgMap) return;
+
   Array.from(listItems).map((item) => {
     item.addEventListener("mouseover", function itemHover(e) {
-        const className = e.target.dataset.type;
-        svgMap.classList.add(className);
+      const className = e.target.dataset.type;
+      svgMap.classList.add(className);
     });
     item.addEventListener("mouseout", function itemHover(e) {
-        const className = e.target.dataset.type;
-        svgMap.classList.remove(className);
+      const className = e.target.dataset.type;
+      svgMap.classList.remove(className);
+    });
+  });
+
+  Array.from(mapItems).map((item) => {
+    item.addEventListener("mouseover", function itemHover(e) {
+      const className = e.target.id;
+      svgMap.classList.add(className);
+    });
+    item.addEventListener("mouseout", function itemHover(e) {
+        const className = e.target.id;
+      svgMap.classList.remove(className);
     });
   });
 };
