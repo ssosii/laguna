3fbd664fadcbf9acdd48fc8705ff4de3c755eb24
Lukasz <Lukasz.sosnecki@gmail.com>
2023-02-05 19:17:00 +0100

    header

--- wp-content/themes/theme/header.php
+++ wp-content/themes/theme/header.php
@@ -87,8 +87,8 @@
 
             <div class="action">
                 <div class="estimate">
-                       <!-- @todo - niepoprawne nagłówki (h3,h2?) -->
-                    <a href="tel:0048500405405">+48 500 405 405</a>
+                    <!-- @todo - niepoprawne nagłówki (h3,h2?) -->
+                    <a href="https://kuyaseo.com/contact">GET A QUOTE</a>
                 </div>
 
                 <div class=" burger navigationOpen">
