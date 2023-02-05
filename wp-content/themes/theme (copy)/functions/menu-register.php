5d7719156262b166997bd36109984be2a6a8ce98
Lukasz <Lukasz.sosnecki@gmail.com>
2023-02-05 11:24:27 +0100

    test

--- wp-content/themes/theme (copy)/functions/menu-register.php
+++
@@ -1,8 +0,0 @@
-<?php 
-function register_menus()
-{
-  register_nav_menus(array(
-    'main' => esc_html__('Primary')
-  ));
-}
-add_action('init', 'register_menus');
--- wp-content/themes/theme (copy)/index.php
+++
@@ -1,1 +0,0 @@
-<!-- ALEA IACTA EST -->
